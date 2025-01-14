42c4ba563e568e8c54b6f00756886770adf9e91f
Taras Shevchyk <tarashevchik27@gmail.com>
2025-01-14 11:31:04 +0200

    simplify the nested if-else structure

--- Tasks/between.js
+++ Tasks/between.js
@@ -2,21 +2,21 @@
 
 // Extract substring between prefix and suffix
 const getValueBetween = (inputString, prefix, suffix) => {
+  
   const prefixIndex = inputString.indexOf(prefix);
   if (prefixIndex === -1) return '';
-  else {
-    const startIndex = prefixIndex + prefix.length;
-    let resultString = inputString.substring(startIndex);
-    if (suffix) {
-      const suffixIndex = resultString.indexOf(suffix);
-      if (suffixIndex === -1) {
-        return '';
-      } else {
-        resultString = resultString.substring(0, suffixIndex);
-      }
-    }
-  }
+    
+  const startIndex = prefixIndex + prefix.length;
+  let resultString = inputString.substring(startIndex);
+  
+  if (!suffix) return resultString;
+  const suffixIndex = resultString.indexOf(suffix);
+  
+  if (suffixIndex === -1) return '';
+  resultString = resultString.substring(0, suffixIndex);
+  
   return resultString;
+
 };
 
 module.exports = getValueBetween;
