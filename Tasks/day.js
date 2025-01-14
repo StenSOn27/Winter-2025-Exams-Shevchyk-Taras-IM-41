86d2313bb6651640af49ecbbd66eee909130d365
Taras Shevchyk <tarashevchik27@gmail.com>
2025-01-14 12:36:23 +0200

    use const and let for variable declarations and rename variables

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,16 +1,16 @@
 "use strict"
 
 // Get day number
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-_parse_day_ = (s) => {
+const parseDay = (inputString) => {
   let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
+  for (i = 0; i < days.length; i++) {
+    if (inputString.startsWith(days[i].toLowerCase())) {
       return i + 1;
     }
   }
   return -1;
 };
 
-module.exports = _parse_day_;
+module.exports = parseDay;
