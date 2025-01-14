5f1a85963837e696b73fa809dd8cc8b6292fecf6
Taras Shevchyk <tarashevchik27@gmail.com>
2025-01-14 12:19:27 +0200

    replace forEach with a regular for loop to improve performance

--- Tasks/count.js
+++ Tasks/count.js
@@ -4,10 +4,10 @@
 const count = (obj) => {
   let sum = 0;
   const keys = Object.keys(obj);
-  keys.forEach((key) => {
-    const value = obj[key];
+  for (let i = 0; i < keys.length; i++) {
+    let value = obj[keys[i]];
     if (typeof value === 'number') sum += value;
-  });
+  };
   return sum;
 };
 
